80e978f3f9159fa6c177a5ac0e6f6235a1c5d157
snehakumari-37 <92073117+snehakumari-37@users.noreply.github.com>
2022-03-10 07:51:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,60 +6,26 @@ if (!badRequest) {
     var end = q[1]; var sub = q[0];
 
 const base = "https://ncert.nic.in/textbook/pdf/";
-
-const names = {
-    1: 'Beehive',       // english Main
-    2: 'Moments',       // english Supplementary
-    3: 'Kshitij',       // hindi Main
-    4: 'Sparsh',        // hindi Supplementary
-    5: 'Mathematics',
-    6: 'Science',
-    7: 'Geography',
-    8: 'Economics',
-    9: 'History',
-    10: 'Political Science'
-};
-
 const books = {
-    1: 'e1',
-    2: 'e2',
-    3: 'h1',
-    4: 'h2',
-    5: 'm',
-    6: 's',
-    7: 'g',
-    8: 'e',
-    9: 'h',
-    10: 'c'
-};
-
-const codes = {
-    1: 'iebe1',
-    2: 'iemo1',
-    3: 'ihks1',
-    4: 'ihkr1',
-    5: 'iemh1',
-    6: 'iesc1',
-    7: 'iess1',         //1 geo
-    8: 'iess2',         //2 economics
-    9: 'iess3',         //3 history
-    10: 'iess4',        //4 civics
-
-};
-
-const totalBooks = Object.getOwnPropertyNames(names).length;
-
-for (var i = 1; i < totalBooks; i++) {
-    
-    if (sub == books[i]) {
-        if(end.length==1){end = '0'+ end;}
-        else if (end == 0) { end = "ps"};
-        break;
+      1:{   name:'Beehive',             subCode:'iebe1',    webCode:'e1'},
+      2:{   name:'Moments',             subCode:'iemo1',    webCode:'e2'},
+      3:{   name:'Kshitij',             subCode:'ihks1',    webCode:'h1'},
+      4:{   name:'Sparsh',              subCode:'ihkr1',    webCode:'h2'},
+      5:{   name:'Mathematics',         subCode:'iemh1',    webCode:'m'},
+      6:{   name:'Science',             subCode:'iesc1',    webCode:'s'},
+      7:{   name:'Geography',           subCode:'iess1',    webCode:'g'},
+      8:{   name:'Economics',           subCode:'iess2',    webCode:'e'},
+      9:{   name:'History',             subCode:'iess3',    webCode:'h'},
+      10:{  name:'Political Science',   subCode:'iess4',    webCode:'c'}
     }
-
-}
-
-var full = base + codes[i] + end  + '.pdf';
+    books.length = Object.getOwnPropertyNames(books).length
+    
+    for(var i=1; i<books.length; i++){
+      if(sub == books[i].webCode){ console.log("found : "+books[i].name); break;}
+    }
+      if(end.length==1){end = '0'+ end;}
+        else if (end == 0) { end = "ps"};
+    var full = base + books[i].subCode + end + '.pdf';
 window.location.replace(full);
 
 }
